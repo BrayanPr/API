82ad00b9621c781b2cf58e936fe34ae2144a0725
Brayan Prieto <a6519150032@utch.edu.mx>
2021-06-02 09:23:24 -0600

    creating Badge

--- cloud-services-project/db_config.py
+++ cloud-services-project/db_config.py
@@ -1,11 +1,9 @@
-
 from flask_pymongo import pymongo
 import os
 
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
+client = pymongo.MongoClient(F"mongodb+srv://{DB_USER}:{DB_PASSWORD}@democluster.z3iq3.mongodb.net/{DB_NAME}?retryWrites=true&w=majority")
 
-client = pymongo.MongoClient(
-    "mongodb+srv://{DB_USER}:{DB_PASSWORD}@cluster0.qobtq.mongodb.net/{DB_NAME}?retryWrites=true&w=majority"
-)
+db = client.Badges
--- cloud-services-project/main.py
+++ cloud-services-project/main.py
@@ -1,9 +1,27 @@
-from flask import Flask, jsonify, request
+from flask import Flask, json, jsonify, request
 from flask_restful import Api, Resource, reqparse, abort
 from flask_pymongo import pymongo
 import db_config as database
 
 app=Flask(__name__)
+api = Api(app)
+
+class Badge(Resource):
+    def post(self):
+        database.db.Badges.insert_one(
+            {
+                'header_img_url': request.json['header_img_url'],
+                'profile_picture_url': request.json['profile_picture_url'],
+                'name': request.json['name'],
+                'age': request.json['age'],
+                'city': request.json['city'],
+                'followers': request.json['followers'],
+                'likes': request.json['likes'],
+                'post': request.json['post']
+            }
+        )
+
+api.add_resource(Bagde,'/new')
 
 if __name__ == '__main__':
     app.run(load_dotenv=True)
