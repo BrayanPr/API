05067fe7c7ec7f816f3bf10a8c910e36195f5904
Brayan Prieto <a6519150032@utch.edu.mx>
2021-06-02 15:16:11 -0600

    updating the Badge class

--- cloud-services-project/db_config.py
+++ cloud-services-project/db_config.py
@@ -4,6 +4,5 @@ import os
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
-client = pymongo.MongoClient(F"mongodb+srv://{DB_USER}:{DB_PASSWORD}@democluster.z3iq3.mongodb.net/{DB_NAME}?retryWrites=true&w=majority")
-
-db = client.Badges
+client = pymongo.MongoClient(F"mongodb+srv://{DB_USER}:{DB_PASSWORD}@demo-database.qobtq.mongodb.net/{DB_NAME}?retryWrites=true&w=majority")
+db = client.db_example
--- cloud-services-project/main.py
+++ cloud-services-project/main.py
@@ -1,15 +1,23 @@
+from re import X
+from bson.objectid import ObjectId
 from flask import Flask, json, jsonify, request
 from flask_restful import Api, Resource, reqparse, abort
 from flask_pymongo import pymongo
+from werkzeug.wrappers import response
 import db_config as database
 
 app=Flask(__name__)
 api = Api(app)
 
 class Badge(Resource):
+
+    def get(self,by,data):
+        response = self.abort_if_not_exist(by,data)
+        response['_id'] = str(response['_id'])
+        return jsonify(response)
+
     def post(self):
-        database.db.Badges.insert_one(
-            {
+        _id = str(database.db.Badges.insert_one({
                 'header_img_url': request.json['header_img_url'],
                 'profile_picture_url': request.json['profile_picture_url'],
                 'name': request.json['name'],
@@ -18,10 +26,25 @@ class Badge(Resource):
                 'followers': request.json['followers'],
                 'likes': request.json['likes'],
                 'post': request.json['post']
-            }
-        )
+            }).inserted_id)
 
-api.add_resource(Bagde,'/new')
+        return jsonify({"_id":_id})
+
+    def put(self, by, data):
+        response = self.abort_if_not_exist(by,data)
+    
+    def abort_if_not_exist(self,by,data):
+        if by == "_id":
+            response = database.db.Badges.find_one({"_id":ObjectId(data)})
+        else:
+            response = database.db.Badges.find_one({f"{by}":data})
+
+        if response:
+            return response
+        else:
+            abort(jsonify({"status":404, f"{by}":f"{data} not found"}))
+
+api.add_resource(Badge,'/new/','/<string:by>:<string:data>/')
 
 if __name__ == '__main__':
     app.run(load_dotenv=True)
